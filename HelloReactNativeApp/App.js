679b727bdcc805913d41da4e45d66c9d031685d8
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-03-13 20:13:13 +0300

    stylesheet

--- HelloReactNativeApp/App.js
+++ HelloReactNativeApp/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box, styles.box1]}>
+        <Text style={[styles.text, styles.text3]}>Box</Text>
+      </View>
+
+      <View style={[styles.box, styles.box2]}>
+        <Text style={[styles.text, styles.text1]}>React</Text>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={[styles.text, styles.text2]}>Native</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +25,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: "gray",
+    marginBottom: 10,
+  },
+  box1: {
+    backgroundColor: "dodgerblue",
+  },
+  box2: {
+    backgroundColor: "bisque",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    backgroundColor: "brown",
+    textAlign: "center",
+  },
+  text1: {
+    backgroundColor: "black",
+  },
+  text2: {
+    backgroundColor: "aquamarine",
+    color: "black",
+  },
+  text3: {
+    backgroundColor: "yellow",
+    color: "#000",
   },
 });
