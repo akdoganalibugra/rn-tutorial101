f99240bf1ef2a8f1501251874a9716229a85f59d
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-03-14 14:30:12 +0300

    justifyContent & alignItems

--- HelloReactNativeApp/App.js
+++ HelloReactNativeApp/App.js
@@ -4,13 +4,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.box1}>
-        <View style={styles.child4}></View>
-        <View style={styles.child5}></View>
+        <Text>React Native</Text>
       </View>
       <View style={styles.box2}>
-        <View style={styles.child1}></View>
-        <View style={styles.child2}></View>
-        <View style={styles.child3}></View>
+        <View style={[styles.child, styles.child1]}></View>
+        <View style={[styles.child, styles.child2]}></View>
+        <View style={[styles.child, styles.child3]}></View>
+        <View style={[styles.child, styles.child4]}></View>
       </View>
     </View>
   );
@@ -25,31 +25,39 @@ const styles = StyleSheet.create({
   box1: {
     flex: 1,
     backgroundColor: "gray",
-    flexDirection: "row",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  child4: {
-    flex: 1,
-    backgroundColor: "lightcoral",
-  },
-  child5: {
-    flex: 1,
-    backgroundColor: "lightseagreen",
-  },
+  // child4: {
+  //   flex: 1,
+  //   backgroundColor: "lightcoral",
+  // },
+  // child5: {
+  //   flex: 1,
+  //   backgroundColor: "lightseagreen",
+  // },
   box2: {
     flex: 1,
     backgroundColor: "bisque",
     flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  child: {
+    width: 50,
+    height: 50,
   },
   child1: {
-    flex: 1,
     backgroundColor: "coral",
   },
   child2: {
-    flex: 1,
     backgroundColor: "lightgreen",
   },
   child3: {
-    flex: 1,
     backgroundColor: "lightblue",
+  },
+  child4: {
+    backgroundColor: "lightcoral",
   },
 });
