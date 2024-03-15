5e7d2850830e4f11c65f030876102e36d5e0f30d
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-03-15 23:08:18 +0300

    textInput

--- HelloReactNativeApp/App.js
+++ HelloReactNativeApp/App.js
@@ -1,36 +1,20 @@
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-} from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const App = () => {
-  const handleClick = () => {
-    alert("Hi");
-  };
-
   return (
     <View style={styles.container}>
-      <Button title="Click Me!" onPress={handleClick} />
-      <TouchableOpacity onPress={handleClick}>
-        <Text>Click TouchableOpacity!</Text>
-      </TouchableOpacity>
-
-      <TouchableHighlight
-        onPress={() => alert("Pressed!")}
-        activeOpacity={1}
-        underlayColor={"#DDDDDD"}
-      >
-        <Text>Click TouchableHighlight!</Text>
-      </TouchableHighlight>
-
-      <TouchableWithoutFeedback onPress={handleClick}>
-        <Text>Click TouchableWithoutFeedback!</Text>
-      </TouchableWithoutFeedback>
+      <Image
+        resizeMode="contain"
+        style={{
+          width: "100%",
+          height: 180,
+          borderWidth: 2,
+          borderColor: "black",
+        }}
+        source={{
+          uri: "https://static.independent.co.uk/2022/11/23/11/newFile-4.jpg",
+        }}
+      />
     </View>
   );
 };
